1ae9056c5e4c71af3871a0310adbf042ac23d695
Hari Krishna <110671039+HariKrishna070@users.noreply.github.com>
2023-03-13 18:41:32 +0530

    Create Middle of the Linked List.py

---
+++ leet code questions/Middle of the Linked List.py
@@ -0,0 +1,60 @@
+"""
+QUESTION:Middle of the Linked List
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+"""
+# SOLUTION:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+  def middleNode(self, head: ListNode) -> ListNode:
+    count=0
+    ptr=head
+    while ptr:
+        count+=1
+        ptr=ptr.next
+    res=(count//2)+1 if count%2==0 else ((count-1)//2)+1
+    res_node=head
+    while(res!=1):
+        res_node=res_node.next
+        res-=1
+    return res_node
+
+## another approch
+
+class Solution:
+  def middleNode(self, head: ListNode) -> ListNode:
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+
+    return slow
